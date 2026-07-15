1d1f9e9e560c13a3cf9b8ce05bed5bcd8f802005
K-Hicks13 <kalishahicks@yahoo.co.uk>
2026-07-15 12:08:40 +0200

    Add user input

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # Number Statistics
 print("Number Statistics")
 numbers = list(map(int, input("Please enter the 10 numbers (separated by space): ").split(" ")))
+
+for index, number in enumerate (numbers, start=1):
+    print(f"Number {index}: {number}")
